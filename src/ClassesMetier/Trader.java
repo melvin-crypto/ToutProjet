ace37cca53fbcf1034a748dbfe75b3986258dcc1
melvin <melvin.pierre.mp@gmail.com>
2021-02-08 14:14:18 +0100

    controle 8 fevrier

--- src/ClassesMetier/Trader.java
+++ src/ClassesMetier/Trader.java
@@ -19,6 +19,30 @@ public class Trader
         this.nom = nom;
         lesTraders = new ArrayList<Action>();
     }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public ArrayList<Action> getLesTraders() {
+        return lesTraders;
+    }
+
+    public void setLesTraders(ArrayList<Action> lesTraders) {
+        this.lesTraders = lesTraders;
+    }
     
     
     
